b1eebc9bfbe8afbe9819c623c39838633eb566e9
Kevin <40772386+nexalicious@users.noreply.github.com>
2018-08-15 22:10:37 +0100

    Update run.js

--- run.js
+++ run.js
@@ -2,7 +2,7 @@
 var listID = "5b7350264a1ee71f51a0ce5b";
 var done = false;
 const trello = require("trello");
-    const t = new trello("d59545423a0ba4796dfcde8ebb3bf698", "a5bd16d641398a3468feb405cad242b23a82729849c1cfcffc21329d11977bad");
+    const t = new trello(process.env.API_KEY, process.env.TRELL_TOKEN);
     
   
 	
