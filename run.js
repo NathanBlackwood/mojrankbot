3ebf1898455e2d80b4c2f7d6cc6fe0d8c48d1175
Kevin <40772386+nexalicious@users.noreply.github.com>
2018-08-15 22:50:59 +0100

    Update run.js

--- run.js
+++ run.js
@@ -1,23 +1,26 @@
 //logins -- dont touch
 var listID = "5b7350264a1ee71f51a0ce5b";
+var on = true
 var done = false;
 const trello = require("trello");
     const t = new trello(process.env.API_KEY, process.env.TRELL_TOKEN);
 const rbx = require("roblox-js");
 
     //userinterface -- touch this
-    var targetName = "EthiopianChild3";
-    var targetId = "645346813";
-    var Judge = "Kevin_Hodge";
+    var targetName = "WilliamBlackwood";
+    var targetId = "22859549";
+    var Judge = "Jesus";
     //end userinterface -- STOP TOUCHING
 
 
     //dont touch anything here
-  if (true) {
+  if (on === true) {
             rbx.setRank(3788004, targetId, 1).then(function(response){
             console.log("response", response);
 	  
 	  t.addCard(targetName+":"+targetId, "Added by: Hon. Judge "+Judge, listID);
+		   
+	on = false
 
 
         })
