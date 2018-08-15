dafff91f857b6c04058f7ce9f7572a675e3bae10
Kevin <40772386+nexalicious@users.noreply.github.com>
2018-08-15 23:22:21 +0100

    Update run.js

--- run.js
+++ run.js
@@ -1,6 +1,6 @@
 //logins -- dont touch
 var listID = "5b7350264a1ee71f51a0ce5b";
-var on = true
+var debounce = false
 var done = false;
 const trello = require("trello");
     const t = new trello(process.env.API_KEY, process.env.TRELL_TOKEN);
@@ -15,7 +15,7 @@ const rbx = require("roblox-js");
 
 
     //dont touch anything here
-  if (true) {
+  if (debounce === false) {
         t.addCard(targetName+":"+targetId, "Added by: Hon. "+Judge, listID).then(function(response){
             var cardId = response.id;
             t.addDueDateToCard(cardId, due);
@@ -27,6 +27,7 @@ const rbx = require("roblox-js");
         rbx.setRank(3788004, targetId, 1).then(function(response){
             console.log("response", response);
         })
+	  debounce = true
   }
 	
 rbx.login(process.env.USER, process.env.PASS);
